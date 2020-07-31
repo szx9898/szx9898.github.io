8c2760fd3d4876fbfd301988018625c6a352d0ab
sunzixiang <szx9808@163.com>
2020-07-31 16:40:32 +0800

    Site updated: 2020-07-31 16:40:27

--- js/Lete.js
+++ js/Lete.js
@@ -1,6 +1,6 @@
 // 设置页脚博主会动的心
 $(document).ready(function(e){
-    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> zzxx');
+    $('.copyright').html('<span style="color:#000000">©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 梓里</span>');
 })
 // 添加up酱
 // 添加图片top
