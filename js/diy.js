a3db147509b8c5abcd5f8a6b3e98a156530a1353
sunzixiang <szx9808@163.com>
2020-08-02 19:46:40 +0800

    Site updated: 2020-08-02 19:46:34

--- js/diy.js
+++ js/diy.js
@@ -13,27 +13,15 @@ for (var i = 0; i < menus_item_child.length; i++) {
     menus_item_child[i].style.display = "none";
     menus_expand[i].className += " menus-closed";
 }
-//分类卡片折叠
-var card_category_list = document.getElementsByClassName(
-    "card-category-list child"
-);
-var item = document.getElementsByClassName("card-category-list-item");
-function toggle(t) {
-    var display = t.parentElement.nextSibling.style.display;
-    if (display == "none") {
-        t.parentElement.nextSibling.style.display = "block";
-        t.parentElement.nextSibling.style.height = "100%";
-        t.className = t.className.replace("fa-chevron-up", "fa-chevron-down");
-    } else {
-        t.parentElement.nextSibling.style.display = "none";
-        t.className = t.className.replace("fa-chevron-down", "fa-chevron-up");
-    }
-}
-
-for (var i = 0; i < card_category_list.length; i++) {
-    card_category_list[i].style.display = "none";
-    card_category_list[i].style.transition = "all 1s";
-    card_category_list[i].previousSibling.innerHTML +=
-        '<i class="fa fa-chevron-up menus-expand  menus-closed" aria-hidden="true" style="margin-left:20px;" onclick="toggle(this)"></i>';
-}
-
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('<span style="color:#000000">©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 梓里</span>');
+})
+// 添加up酱
+// 添加图片top
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+// 添加到返回顶部按钮下
+document.getElementById("go-up").innerHTML += top_up;
+// 移除页脚样式
+// var app = document.getElementById('footer');
+// app.removeAttribute('id')
