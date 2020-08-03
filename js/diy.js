8414acc0c1272d3cf06489edd9a9bc6ec359396d
sunzixiang <szx9808@163.com>
2020-08-03 09:02:29 +0800

    Site updated: 2020-08-03 09:02:23

--- js/diy.js
+++ js/diy.js
@@ -1,3 +1,16 @@
+// 设置页脚博主会动的心
+$(document).ready(function(e){
+    $('.copyright').html('©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 梓里');
+})
+// 添加up酱
+// 添加图片top
+var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
+// 添加到返回顶部按钮下
+document.getElementById("go-up").innerHTML += top_up;
+// 移除页脚样式
+// var app = document.getElementById('footer');
+// app.removeAttribute('id')
+
 // 背景变透明
 // var full_page = document.getElementsByClassName("full_page");
 // if (full_page.length != 0) {
@@ -13,15 +26,3 @@ for (var i = 0; i < menus_item_child.length; i++) {
     menus_item_child[i].style.display = "none";
     menus_expand[i].className += " menus-closed";
 }
-// 设置页脚博主会动的心
-$(document).ready(function(e){
-    $('.copyright').html('<span style="color:#000000">©2020 <i style="color:#FF6A6A;animation: announ_animation 0.8s linear infinite;" class="fa fa-heartbeat"></i> 梓里</span>');
-})
-// 添加up酱
-// 添加图片top
-var top_up = "<img id='upj' class='upj' style='max-width: 1000%; transform: translate(-70px,-70px);' src='https://cdn.jsdelivr.net/gh/lete114/CDN/Use/up.gif' title='回到顶部' >";
-// 添加到返回顶部按钮下
-document.getElementById("go-up").innerHTML += top_up;
-// 移除页脚样式
-// var app = document.getElementById('footer');
-// app.removeAttribute('id')
